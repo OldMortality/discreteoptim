2ceec86ea709c9c32dbce6f9fc97442fe87973da
OldMortality <michel_de_lange@yahoo.co.uk>
2020-06-14 18:25:19 +1200

    improved optimistic

---
+++ assignment2b/knapsack/BBSolver.java
@@ -0,0 +1,231 @@
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * 
+ * Branch and Bound Solver
+ * 
+ * 
+ */
+public class BBSolver {
+
+	public static void printArray(int[] a) {
+		System.out.print("[ ");
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.println("]");
+	}
+
+	private int bestValue = 0;
+
+	private int[] bestItems;
+
+	private void setBestValue(int bestValue) {
+		this.bestValue = bestValue;
+	}
+
+	private void setBestItems(int[] bestIs) {
+		// System.out.println("best items" + bestItems[1]+ " " + bestItems[2]);
+		this.bestItems = bestIs.clone();
+	}
+
+	private int getBestValue() {
+		return (this.bestValue);
+	}
+
+	private int[] getBestItems() {
+		return (this.bestItems);
+	}
+
+	private void setBest(int value, int[] items) {
+		// printArray(items);
+		// System.out.println(value);
+		if (value > this.getBestValue()) {
+			setBestValue(value);
+			setBestItems(items);
+
+		}
+	}
+
+	private void initBest(int n) {
+		setBestValue(0);
+		int[] bestIt = new int[n];
+		setBestItems(bestIt);
+	}
+
+	/*
+	 * @param value : The total value until the current node
+	 * 
+	 * @param valuesToGo: all values of all nodes below this one
+	 * 
+	 * @param weightsToGo: Weights of all nodes below this one
+	 * 
+	 * @parm capacity: total size of the knapsack
+	 * 
+	 * Get an optimistic upper bound on the value we can have in the knapsack. This
+	 * one simply assumes everything will fit in.
+	 */
+	int getOptimistic(int value, int capacityLeft, int[] valuesToGo, int[] weigthsToGo) {
+
+		int sum = 0;
+		for (int i : valuesToGo)
+			sum += i;
+		return (value + sum);
+
+	}
+
+	/*
+	 * @param value : The total value until the current node
+	 * 
+	 * @param valuesToGo: all values of all nodes below this one
+	 * 
+	 * @param weightsToGo: Weights of all nodes below this one
+	 * 
+	 * @parm capacity: total size of the knapsack
+	 * 
+	 * @returns float: uppber bound on total value
+	 * 
+	 * Get an optimistic upper bound on the value we can have in the knapsack
+	 * 
+	 * As many of the next items as will fit as a whole, plus the fraction of the
+	 * next 'Belgian chocolate model'. Includes the value already in the knapsack
+	 * before you got to this item.
+	 * 
+	 */
+	float getOptimisticLinear(int value, int capacityLeft, int[] valuesToGo, int[] weightsToGo) {
+
+		// the total value if we add what we can into the knapsack
+		float sum = (float) value;
+		// weight we are adding now
+		int weightAdded = 0;
+		int i = 0;
+		for (i = 0; i < weightsToGo.length; i++) {
+			if (weightAdded + weightsToGo[i] <= capacityLeft) {
+				weightAdded += weightsToGo[i];
+				sum += valuesToGo[i];
+			} else {
+				if (i < weightsToGo.length ) {
+					// take a piece of the item, if there are any
+					float prop = (float) (capacityLeft - weightAdded) / weightsToGo[i];
+					sum += prop * valuesToGo[i ];
+					continue;
+				}
+			}
+		}
+		return (sum);
+	}
+
+	public void goElement(int ix, int value, int k_used, int[] items, int K, int[] v, int[] w) {
+
+		// items = items.clone();
+		// printArray(items);
+		if (ix < items.length) {
+
+			if (k_used + w[ix] <= K) {
+
+				if (getOptimistic(value, K, Arrays.copyOfRange(v, ix, v.length),
+						Arrays.copyOfRange(w, ix, w.length)) > getBestValue()) {
+					// There may be a point in taking this element
+					// try taking it
+					items[ix] = 1;
+					goElement(ix + 1, value + v[ix], k_used + w[ix], items, K, v, w);
+					// try not taking it
+					items[ix] = 0;
+					goElement(ix + 1, value, k_used, items, K, v, w);
+				} else {
+					// it is pointless to take this element
+					items[ix] = 0;
+				}
+			} else {
+				// try not taking it
+				items[ix] = 0;
+				goElement(ix + 1, value, k_used, items, K, v, w);
+			}
+		} else {
+			// leaf node
+			setBest(value, items);
+		}
+
+	}
+
+	public String createOutput(ArraySorter sorter) {
+
+		// we need the sorter, so we can retrieve the
+		// order of the elements, as it was before sorting by
+		// value density.
+		Integer[] ix = sorter.getIndexes();
+		// this is the array of items to take (0,1) in original order
+		Integer[] bestItemsOriginal = new Integer[ix.length];
+		for (int i = 0; i < bestItems.length; i++) {
+			bestItemsOriginal[ix[i]] = bestItems[i];
+		}
+		String line1 = getBestValue() + " 0\n";
+		int[] bestIts = getBestItems();
+		String line2 = "";
+		for (int i = 0; i < bestIts.length; i++) {
+			line2 = line2 + bestItemsOriginal[i] + " " ;
+		}
+		return (line1 + line2);
+	}
+
+	/*
+	public Float[] getValueDensity(int[] values,int[] weights) {
+		Float[] result = new Float[values.length]; 
+		for (int i=0; i < values.length;i++) {
+			float f = ( (float)  values[i] )/ ( (float) weights[i]  );
+			result[i] = Float.valueOf( f );
+		}
+		return(result);
+	}
+	
+	public void sortMyArrays(Float[] valueDensity,int[] values, int[] weights) {
+		ArrayIndexComparator comparator = new ArrayIndexComparator(valueDensity);
+		Integer[] indexes = comparator.createIndexArray();
+		// indexes is now ordered in ascending valueDensity
+		Arrays.sort(indexes, comparator);
+		int[] valuesSorted = new int[values.length];
+		int[] weightsSorted = new int[weights.length];
+		for (int i=0;i<indexes.length;i++) {
+			valuesSorted[i] = values[indexes[i]];
+			weightsSorted[i] = weights[indexes[i]]; 
+		}
+		
+	}
+	*/
+	
+	public String solve(int K, int[] values, int[] weights) {
+
+		initBest(values.length);
+		ArraySorter sorter = new ArraySorter(values, weights);
+		sorter.sortByValueDensity();
+		int[] valuesSorted = sorter.getSortedValues();
+		int[] weightsSorted = sorter.getSortedWeights();
+		
+		int[] itemsTaken = new int[valuesSorted.length];
+		for (int i = 0; i < valuesSorted.length; i++) {
+			itemsTaken[i] = 0;
+		}
+		goElement(0, 0, 0, itemsTaken, K, valuesSorted, weightsSorted);
+		return (this.createOutput(sorter));
+
+	}
+
+	public static void main(String[] args) {
+		BBSolver bb = new BBSolver();
+		int[] weights = { 2, 2, 2, 5, 5, 8, 3 };
+		int[] values = { 1, 1, 1, 10, 10, 13, 7 };
+		int K = 10;
+		String output = bb.solve(K, values, weights);
+		System.out.println(output);
+		
+		int i = 3;
+		int j = 4;
+		float f = ((float) i ) / ((float) j);
+		System.out.println("f " + f);
+
+	}
+
+}
+
--- assignment2b/knapsack/solver.py
+++ assignment2b/knapsack/solver.py
@@ -1,149 +1,40 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import numpy as np
-import sys
- 
 
-from collections import namedtuple
-Item = namedtuple("Item", ['index', 'value', 'weight'])
-
- 
-def getBest():
-    return(bestValue)
-
- 
-def setBest(x,arr):
-    global bestValue
-    if (x > bestValue):
-        #print('set best to ', x)
-        bestValue = x
-        setBestItems(arr)
-
- 
-def initBest():
-    global bestValue
-    bestValue = 0    
-
- 
-def initBestItems(n):
-    global bestItems
-    bestItems=np.zeros(n, dtype=np.int)
-
- 
-def setBestItems(arr):
-    global bestItems
-    #print('best items' , bestItems)
-    bestItems = np.array(arr, dtype=np.int)
-        
- 
-def getBestItems():
-    return(bestItems)
-
- 
- 
-
- 
-def goElement(ix,value,k_used,items,K,v,w):
-
-    if ix < items.size :
-        #print(ix)python -m nuitka --standalone --mingw64 hello.py
-         # it is feasible to take this element
-        if (k_used + w[ix] <= K):
-            if value + v[ix:].sum() > getBest():
-                # There may be a point in taking this element
-                # try taking it
-                items[ix] = 1
-                goElement(ix+1,value + v[ix],k_used + w[ix],items,K,v,w)  
-                # try not taking it
-                items[ix] = 0
-                goElement(ix+1,value ,k_used,items,K,v,w)  
-            else:
-                # it is pointless to take this element
-                items[ix] = 0
-        else:
-            # try not taking it
-            items[ix] = 0
-            goElement(ix+1,value ,k_used,items,K,v,w)
-        
-    else:
-        # end node
-        #print(ix,value,k_used,items)
-        setBest(value,items)
-
+import os
+from subprocess import Popen, PIPE
 
 def solve_it(input_data):
-    # Modify this code to run your optimization algorithm
 
-    # parse the input
-    lines = input_data.split('\n')
+    # Writes the inputData to a temporay file
 
-    firstLine = lines[0].split()
-    item_count = int(firstLine[0])
-    capacity = int(firstLine[1])
+    tmp_file_name = 'tmp.data'
+    tmp_file = open(tmp_file_name, 'w')
+    tmp_file.write(input_data)
+    tmp_file.close()
 
-    all_items = []
+    # Runs the command: java Solver -file=tmp.data
 
-    for i in range(1, item_count+1):
-        line = lines[i]
-        parts = line.split()
-        all_items.append(Item(i-1, int(parts[0]), int(parts[1])))
+    #print('running solver.py')
 
-    it = np.array(all_items)
-    v = it[:,1]
-    w = it[:,2]
-    K = capacity
-    
-    value_density = v/w
-    
-    arr1inds = value_density.argsort()[::-1]
-    sorted_v = v[arr1inds]
-    sorted_w = w[arr1inds]
-    v= sorted_v
-    w= sorted_w
-    
-    n = w.size
-    
-   
-    sys.setrecursionlimit(n + 100)
-    #items = np.zeros(n)
+    process = Popen(['java', '-Xmx4096m', 'Solver', '-file=' + tmp_file_name], stdout=PIPE, universal_newlines=True)
+    (stdout, stderr) = process.communicate()
 
-    #bestValue = 0
-    initBest()
-    initBestItems(n)
-    goElement(0,0,0,np.zeros(n),K,v,w)
-    getBest()
-    
-    value = getBest()
-    
-    # prepare the solution in the specified output format
-    output_data = str(value) + ' ' + str(0) + '\n'
-    t = getBestItems()
-       # reorder items back again
-    ix_list = list(arr1inds)
-    a = ''
-    for j in range(0,t.size):
-        ix = ix_list.index(j)
-        a = a + str(t[ix])  
-    
-    output_data += ' '.join(map(str, a))
-   
-    #trueOutput = (v2 * t2).sum()
-    #print('trueOutput',trueOutput)
-    #trueWeight = (w2 * t2).sum()
-    #print('trueWeight',trueWeight)
-    
-    
-    return(output_data)
+    # removes the temporay file
+    os.remove(tmp_file_name)
 
+    return stdout.strip()
+
+
+import sys
 
 if __name__ == '__main__':
-    import sys
-    if len(sys.argv) > 0:
-        
-        file_location =  sys.argv[1].strip()  #'data/ks_30_0' 
+   
+    if len(sys.argv) > 1:
+        file_location = sys.argv[1].strip()
         with open(file_location, 'r') as input_data_file:
             input_data = input_data_file.read()
         print(solve_it(input_data))
-    #else:
-    #    print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)')
+    else:
+        print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)')
 
